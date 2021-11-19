4f03c83edeceb8a016cca1b5b48d434d48fde93c
bharathnw <msreebharath21@gmail.com>
2021-11-19 12:13:53 -0600

    changes done related to passport

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var indexRouter = require('./routes/index');
 var georgiaRouter = require('./routes/georgia');
 var addModsRouter = require('./routes/addmods');
@@ -74,6 +75,34 @@ if (reseed) { recreateDB(); }
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        Account.findOne({ username: username }, function(err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false, { message: 'Incorrect username.' });
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Incorrect password.' });
+            }
+            return done(null, user);
+        });
+    }))
+
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use(logger('dev'));
 app.use(express.json());

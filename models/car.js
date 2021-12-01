e43c050f671961ff33b3aa9e2eb67957e7891952
bharathnw <msreebharath21@gmail.com>
2021-12-01 17:43:38 -0600

    changes related to gaurd

--- models/car.js
+++ models/car.js
@@ -1,8 +1,17 @@
 const mongoose = require("mongoose");
 const carSchema = mongoose.Schema({
-    name: String,
-    color: String,
-    cost: Number
+    name: {
+        type: String,
+        minLength: 3
+    },
+    color: {
+        type: String,
+        minLength: 3
+    },
+    cost: {
+        type: Number,
+        min: 3
+    }
 });
 
 module.exports = mongoose.model("Car", carSchema);
--- routes/car.js
+++ routes/car.js
@@ -18,13 +18,13 @@ router.get('/detail', car_controlers.car_view_one_Page);
 
 
 //For Crete
-router.get('/create', car_controlers.car_create_Page);
+router.get('/create', secured, car_controlers.car_create_Page);
 
 
 //For Update
 router.get('/update', secured, car_controlers.car_update_Page);
 
 //For Delete
-router.get('/delete', car_controlers.car_delete_Page);
+router.get('/delete', secured, car_controlers.car_delete_Page);
 
 module.exports = router;

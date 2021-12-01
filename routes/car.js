8d7c6e5c88fd201e816a90d22367ea8901362acd
bharathnw <msreebharath21@gmail.com>
2021-12-01 13:08:24 -0600

    changes related to authorisation

--- routes/car.js
+++ routes/car.js
@@ -2,6 +2,14 @@ var express = require('express');
 const car_controlers = require('../controllers/car');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
+
 /* GET cars */
 router.get('/', car_controlers.car_view_all_Page);
 
@@ -14,7 +22,7 @@ router.get('/create', car_controlers.car_create_Page);
 
 
 //For Update
-router.get('/update', car_controlers.car_update_Page);
+router.get('/update', secured, car_controlers.car_update_Page);
 
 //For Delete
 router.get('/delete', car_controlers.car_delete_Page);
